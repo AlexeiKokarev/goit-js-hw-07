bac9b25eefe80f00fb088de5391776403aa9b11d
AlexeiKokarev <87031896+AlexeiKokarev@users.noreply.github.com>
2021-12-17 14:45:31 +0200

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerEl = document.querySelector('.gallery');
+const markup = galleryItems
+    .map(element => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${element.original}">
+    <img class="gallery__image" 
+    src="${element.preview}"
+    data-source="${element.original}"
+    alt="${element.description}">
+    </a>
+    </div>`;
+    })
+    .join('');
+containerEl.insertAdjacentHTML('afterbegin', markup);
+containerEl.addEventListener('click', onClickImage);
+
+function onClickImage(event) {
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+     <img src="${event.target.dataset.source}"/>`, {
+        onShow: (instance) => {
+            window.addEventListener('keydown', onEscClose);
+        },
+        onClose: (instance) => {
+            window.removeEventListener('keydown', onEscClose)
+        }
+    })
+
+    instance.show();
+
+    function onEscClose(event) {
+        if (event.code === 'Escape') {
+            instance.close()
+        }
+    };
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const containerEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+    .map(element => {
+        return `<div class="gallery__item">
+    <a class="gallery__item" href="${element.original}">
+    <img class="gallery__image"
+    src="${element.preview}"
+     alt="${element.description}" 
+     title = "${element.description}" 
+    captionDelay = 250ms>
+    </a>
+    </div>`;
+    })
+    .join('');
+containerEl.insertAdjacentHTML('afterbegin', markup);
+
+const gallery = new simpleLightbox('.gallery__item a');
+gallery.on('show.simpleLightbox', function() {
+
+});
